f5f40bae6915018e2f972b86867eb0106cb82eb4
shubham.shirore <shubham.shirore@Dev12.glodom.local>
2018-10-03 18:31:46 +0530

    something changed

--- DemoRestSevice/src/main/java/com/student/DemoRestSevice/DemoRestSeviceApplication.java
+++ DemoRestSevice/src/main/java/com/student/DemoRestSevice/DemoRestSeviceApplication.java
@@ -19,8 +19,7 @@ public class DemoRestSeviceApplication {
 		a=1;b=5;c=a+b;
 		System.out.println("a+b="+ c);
 		//comment added
-		//one more comment added
-		//third jdslkgf;hlwre
-
+	
+		System.out.println("Hello Shubham!!!!!!!!!!!!!!!!!!!!!11");
 	}
 }
