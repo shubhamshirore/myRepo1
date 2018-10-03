d65f5ab06d648f5f08aa0a72cb245185e10160b1
shubham.shirore <shubham.shirore@Dev12.glodom.local>
2018-10-03 12:43:10 +0530

    one more syso added

--- DemoRestSevice/src/main/java/com/student/DemoRestSevice/DemoRestSeviceApplication.java
+++ DemoRestSevice/src/main/java/com/student/DemoRestSevice/DemoRestSeviceApplication.java
@@ -9,5 +9,6 @@ public class DemoRestSeviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoRestSeviceApplication.class, args);
 		System.out.println("Hello Nachiket");
+		System.out.println("Hello Shubham");
 	}
 }
